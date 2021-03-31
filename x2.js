5dbb61cb9d747293a1d3da0b2c6645a89b72539d
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-03-31 17:02:37 +0200

    added picture input and display

--- x2.js
+++ x2.js
@@ -5,3 +5,17 @@ document.querySelectorAll(".subContainer").forEach(subContainer => subContainer.
 document.querySelectorAll(".subContainer").forEach(subContainer => subContainer.addEventListener('mouseleave', function(){
     subContainer.style.backgroundColor = "green";
 }))
+
+
+let loadFile = (event) => {
+    let _output1 = document.getElementById('moon1');
+    let _output2 = document.getElementById('moon2');
+    let _output3 = document.getElementById('moon3');
+    let _output4 = document.getElementById('moon4');
+    let _output5 = document.getElementById('moon5');
+    _output1.src = URL.createObjectURL(event.target.files[0]);
+    _output2.src = URL.createObjectURL(event.target.files[0]);
+    _output3.src = URL.createObjectURL(event.target.files[0]);
+    _output4.src = URL.createObjectURL(event.target.files[0]);
+    _output5.src = URL.createObjectURL(event.target.files[0]);
+}
