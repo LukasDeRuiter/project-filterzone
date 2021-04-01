bdd3b39e2696caeb70aad04d2dc53b7897083cd9
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-04-01 11:31:55 +0200

    implemented the .random from lodash as practice and the css filter on click

--- x2.js
+++ x2.js
@@ -1,6 +1,10 @@
+
 document.querySelectorAll(".subContainer").forEach(subContainer => subContainer.addEventListener('mouseenter', function(){
     subContainer.style.backgroundColor = "purple";
-}))
+    for(i = 1; i < 6; i++){
+        document.getElementById(`randomNum${i}`).innerHTML = `filter #${_.random(1, 255)}`;
+
+}}))
 
 document.querySelectorAll(".subContainer").forEach(subContainer => subContainer.addEventListener('mouseleave', function(){
     subContainer.style.backgroundColor = "green";
@@ -19,3 +23,19 @@ let loadFile = (event) => {
     _output4.src = URL.createObjectURL(event.target.files[0]);
     _output5.src = URL.createObjectURL(event.target.files[0]);
 }
+
+document.getElementById('moon1').addEventListener('click', function(){
+    document.getElementById('moon1').style.webkitFilter = "grayscale(100%)";
+})
+document.getElementById('moon2').addEventListener('click', function(){
+    document.getElementById('moon2').style.webkitFilter = "saturate(8)";
+})
+document.getElementById('moon3').addEventListener('click', function(){
+    document.getElementById('moon3').style.webkitFilter = "hue-rotate(90deg)";
+})
+document.getElementById('moon4').addEventListener('click', function(){
+    document.getElementById('moon4').style.webkitFilter = "blur(5px)";
+})
+document.getElementById('moon5').addEventListener('click', function(){
+    document.getElementById('moon5').style.webkitFilter = "invert(100%)";
+})
