678f0d448e4014c6926867c9475b8b328fbfce53
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-04-02 20:36:06 +0200

    finished the canvas, so now the pictures with filters are downloadable and the canvas scales with the image

--- x2.js
+++ x2.js
@@ -10,6 +10,8 @@ document.querySelectorAll(".subContainer").forEach(subContainer => subContainer.
     subContainer.style.backgroundColor = "green";
 }))
 
+let imageHolder;
+let chosenFilter = "";
 
 let loadFile = (event) => {
     let _output1 = document.getElementById('moon1');
@@ -22,16 +24,20 @@ let loadFile = (event) => {
     _output3.src = URL.createObjectURL(event.target.files[0]);
     _output4.src = URL.createObjectURL(event.target.files[0]);
     _output5.src = URL.createObjectURL(event.target.files[0]);
+    console.log(event.target.files[0]);
     let reader = new FileReader();
     reader.readAsDataURL(event.target.files[0]);
     reader.onloadend = function (e) {
         let img = new Image();
         img.src = e.target.result;
+        imageHolder = e.target.result;
         img.onload = function(ev){
             let canvas = document.getElementById('downloadImg');
             let ctx = canvas.getContext('2d');
-            ctx.filter = "grayscale(100%)";
-            ctx.drawImage(img, 0, 0, img.height, img.width);
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.filter = `${chosenFilter}`; 
+            ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width , img.height);
             let imgData = canvas.toDataURL("image/jpeg",0.75);
         }
     }
@@ -39,18 +45,78 @@ let loadFile = (event) => {
 
 document.getElementById('moon1').addEventListener('click', function(){
     document.getElementById('moon1').style.webkitFilter = "grayscale(100%)";
+    chosenFilter = "grayscale(100%)";
+    let img = new Image();
+    img.src = imageHolder;
+    img.onload = function(ev){
+        let canvas = document.getElementById('downloadImg');
+        let ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.filter = `${chosenFilter}`; 
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width , img.height);
+        let imgData = canvas.toDataURL("image/jpeg",0.75);
+    }
 })
 document.getElementById('moon2').addEventListener('click', function(){
     document.getElementById('moon2').style.webkitFilter = "saturate(8)";
+    chosenFilter = "saturate(8)";
+    let img = new Image();
+    img.src = imageHolder;
+    img.onload = function(ev){
+        let canvas = document.getElementById('downloadImg');
+        let ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.filter = `${chosenFilter}`; 
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width , img.height);
+        let imgData = canvas.toDataURL("image/jpeg",0.75);
+    }
 })
 document.getElementById('moon3').addEventListener('click', function(){
     document.getElementById('moon3').style.webkitFilter = "hue-rotate(90deg)";
+    chosenFilter = "hue-rotate(90deg)";
+    let img = new Image();
+    img.src = imageHolder;
+    img.onload = function(ev){
+        let canvas = document.getElementById('downloadImg');
+        let ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.filter = `${chosenFilter}`; 
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width , img.height);
+        let imgData = canvas.toDataURL("image/jpeg",0.75);
+    }
 })
 document.getElementById('moon4').addEventListener('click', function(){
     document.getElementById('moon4').style.webkitFilter = "blur(5px)";
+    chosenFilter = "blur(5px)";
+    let img = new Image();
+    img.src = imageHolder;
+    img.onload = function(ev){
+        let canvas = document.getElementById('downloadImg');
+        let ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.filter = `${chosenFilter}`; 
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width , img.height);
+        let imgData = canvas.toDataURL("image/jpeg",0.75);
+    }
 })
 document.getElementById('moon5').addEventListener('click', function(){
     document.getElementById('moon5').style.webkitFilter = "invert(100%)";
+    chosenFilter = "invert(100%)";
+    let img = new Image();
+    img.src = imageHolder;
+    img.onload = function(ev){
+        let canvas = document.getElementById('downloadImg');
+        let ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.filter = `${chosenFilter}`; 
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width , img.height);
+        let imgData = canvas.toDataURL("image/jpeg",0.75);
+    }
 })
 
 
